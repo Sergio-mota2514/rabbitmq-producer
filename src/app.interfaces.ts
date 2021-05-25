3eeeeb59bbe17b3f77cfdbe812677d04a1d73107
Sergio Mota <sergio.mota2514@gmail.com>
2021-05-25 15:48:26 -0300

    adjust/apiProperty

--- src/app.interfaces.ts
+++ src/app.interfaces.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class PostBodyDto {
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The object to send to RabbitMQ queue.',
+    minimum: 1,
+    default: { propertyExample: 'valueExample' },
+  })
   obj: object;
 }
